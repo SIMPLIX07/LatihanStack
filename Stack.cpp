420e4f402d038a545e9e1bd7bc0c644a7cab89cc
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-08-13 13:17:56 +0700

    penambahan logika pemilihan menu pada main, penambahan procedure printList, dan penambahan color text

--- Stack.cpp
+++ Stack.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 struct piring {
-    string punyaOrang;
+    int nomor;
 };
 
 typedef piring infotype;
@@ -38,8 +38,14 @@ address createElmtList(infotype data) {
 }
 
 void push(list &L, address P) {
+    if(L.top == nil){
+        L.top = P;
+        cout << "\033[32mPiring pertama berhasil ditambahkan\033[0m" << endl;
+        return;
+    }
     P->next = L.top;
     L.top = P;
+    cout << "\033[32mPiring berhasil ditambahkan\033[0m" << endl;
 }
 
 void pop(list &L) {
@@ -47,6 +53,9 @@ void pop(list &L) {
         address P = L.top;
         L.top = L.top->next;
         delete P;
+        cout << "\033[32mPiring berhasil diambil\033[0m" << endl;
+    }else{
+        cout << "\033[31mTidak ada piring yang bisa digunakan\033[0m" << endl;
     }
 }
 
@@ -54,7 +63,66 @@ bool isEmpty(const list &L) {
     return L.top == nil;
 }
 
+void printList(list &L){
+    if(isEmpty(L)){
+        cout << "\033[31mTidak ada piring yang bisa digunakan\033[0m" << endl;
+        return;
+    }
+    cout << "\033[33mPiring diurutkan dari tumpukan paling atas\033[0m" << endl;
+    int i = 1;
+    address p = L.top;
+    while(p != nil){
+        cout << i << ". " << p->info.nomor << endl;
+        p = p->next;
+        i++;
+    }
+    cout << "\033[32mTotal piring yang tersedia berjumlah: \033[0m" << i-1 << endl;
+}
+
+    void printMenu() {
+    cout << "===============================" << endl;
+    cout << "          Piring Restorant        " << endl;
+    cout << "===============================" << endl;
+    cout << "1. Tambah piring bersih" << endl;
+    cout << "2. Ambil piring" << endl;
+    cout << "3. Lihat semua piring" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "===============================" << endl;
+    cout << "\033[33mPilih menu: \033[0m"; 
+}
+
+
 int main() {
-    cout << "Haloo";
+    list L;
+    int input, nomor;
+    createList(L);
+    do{
+        printMenu();
+        cin >> input;
+        switch(input){
+            case 1:{
+                cout << "Masukan nomor piring: ";
+                cin >> nomor;
+                infotype piring = {nomor};
+                address P = createElmtList(piring);
+                push(L, P);
+                break;
+            }
+            case 2:{
+                pop(L);
+                break;
+            }
+            case 3:{
+                printList(L);
+                break;
+            }
+            case 0:{
+                cout << "\033[32mTerima kasih sudah menggunakan layanan kami\033[0m" << endl;
+                break;
+            }
+            default:
+                cout << "\033[31mInput tidak valid\033[0m" << endl;
+        }
+    }while (input != 0);
     return 0;
 }
